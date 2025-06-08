e182010dc889bde8da135d3171c7f4af0b98952a
Travis CI <yadusurya666@gmail.com>
2025-06-08 13:36:27 +0530

    fixed error

--- components/product/product-home-layout-2.tsx
+++ components/product/product-home-layout-2.tsx
@@ -83,8 +83,8 @@ const ProductHomeGrid2 = () => {
     <div className="container mx-auto px-4 my-10">
       <h2 className="mb-6 text-2xl font-bold">Latest</h2>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
-        {products.map((product:any) => (
-          <div key={product._id} className="w-full">
+        {products.map((product:any,index) => (
+          <div key={index} className="w-full">
             <ProductCard3 product={product} />
           </div>
         ))}
