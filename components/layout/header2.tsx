76d04812bb21c1ebcd14f532fc10fad72fe72419
Travis CI <yadusurya666@gmail.com>
2025-11-20 19:50:15 +0530

    Website updated

--- components/layout/header2.tsx
+++ components/layout/header2.tsx
@@ -50,8 +50,9 @@ const Header2 = () => {
   };
 
   return (
-    <header
-      className={`fixed border-b top-0 w-full z-50 transition-all duration-300 ${
+    <div>
+      <header
+      className={`fixed  top-0 w-full z-50 transition-all duration-300 ${
         isScrolled
           ? "bg-background/90 backdrop-blur-md shadow-sm"
           : "bg-background"
@@ -119,14 +120,14 @@ const Header2 = () => {
                     initial={{ x: -20, opacity: 0 }}
                     animate={{ x: 0, opacity: 1 }}
                     transition={{ delay: 0.1 }}
-                    className="flex items-center gap-2"
+                    className="flex items-center gap-2 text-sm border rounded-full  focus:ring-black"
                   >
                     <input
                       type="text"
                       value={searchTerm}
                       onChange={(e) => setSearchTerm(e.target.value)}
                       placeholder="Search products..."
-                      className="flex-1 px-4 py-2 text-sm border rounded-full focus:outline-none focus:ring-1 focus:ring-black"
+                      className="flex-1 px-4 py-2 focus:outline-none "
                     />
                     <motion.button
                       type="submit"
@@ -239,6 +240,10 @@ const Header2 = () => {
         </AnimatePresence>
       </div>
     </header>
+    <div className="h-18">
+
+    </div>
+    </div>
   );
 };
 
